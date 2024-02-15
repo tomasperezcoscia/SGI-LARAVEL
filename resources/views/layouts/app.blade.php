0b997dcaae764f62efb8391e3fac6e5a4edb064c
tomasperezcoscia <perezcosciatomas@gmail.com>
2024-02-15 19:12:09 -0300

    wtf is this?

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,8 @@
 
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/SGI-LARAVEL/build/assets/app-lhA9k1qk.css">
+    <script src="/SGI-LARAVEL/build/assets/app-Sy-x52Gu.js"></script>
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
         crossorigin="anonymous"></script>
